5339210a5c49faddccdbc0482e8d6cc8fe3217f1
smithakolan <37849431+smithakolan@users.noreply.github.com>
2018-04-25 10:59:09 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace CategoryProducts
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstCategories = new System.Windows.Forms.ListBox();
+            this.lstProducts = new System.Windows.Forms.ListBox();
+            this.lblCategories = new System.Windows.Forms.Label();
+            this.lblProducts = new System.Windows.Forms.Label();
+            this.northwindDataSet1 = new CategoryProducts.NorthwindDataSet();
+            this.categoriesTableAdapter1 = new CategoryProducts.NorthwindDataSetTableAdapters.CategoriesTableAdapter();
+            this.productsTableAdapter1 = new CategoryProducts.NorthwindDataSetTableAdapters.ProductsTableAdapter();
+            this.tableAdapterManager1 = new CategoryProducts.NorthwindDataSetTableAdapters.TableAdapterManager();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lstCategories
+            // 
+            this.lstCategories.FormattingEnabled = true;
+            this.lstCategories.Location = new System.Drawing.Point(68, 115);
+            this.lstCategories.Name = "lstCategories";
+            this.lstCategories.Size = new System.Drawing.Size(346, 368);
+            this.lstCategories.TabIndex = 0;
+            this.lstCategories.SelectedIndexChanged += new System.EventHandler(this.lstCategories_SelectedIndexChanged);
+            // 
+            // lstProducts
+            // 
+            this.lstProducts.FormattingEnabled = true;
+            this.lstProducts.Location = new System.Drawing.Point(478, 115);
+            this.lstProducts.Name = "lstProducts";
+            this.lstProducts.Size = new System.Drawing.Size(346, 368);
+            this.lstProducts.TabIndex = 1;
+            // 
+            // lblCategories
+            // 
+            this.lblCategories.AutoSize = true;
+            this.lblCategories.Font = new System.Drawing.Font("Sitka Small", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblCategories.Location = new System.Drawing.Point(129, 61);
+            this.lblCategories.Name = "lblCategories";
+            this.lblCategories.Size = new System.Drawing.Size(170, 40);
+            this.lblCategories.TabIndex = 2;
+            this.lblCategories.Text = "Categories";
+            this.lblCategories.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblProducts
+            // 
+            this.lblProducts.AutoSize = true;
+            this.lblProducts.Font = new System.Drawing.Font("Sitka Small", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblProducts.Location = new System.Drawing.Point(485, 61);
+            this.lblProducts.Name = "lblProducts";
+            this.lblProducts.Size = new System.Drawing.Size(143, 40);
+            this.lblProducts.TabIndex = 3;
+            this.lblProducts.Text = "Products";
+            // 
+            // northwindDataSet1
+            // 
+            this.northwindDataSet1.DataSetName = "NorthwindDataSet";
+            this.northwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // categoriesTableAdapter1
+            // 
+            this.categoriesTableAdapter1.ClearBeforeFill = true;
+            // 
+            // productsTableAdapter1
+            // 
+            this.productsTableAdapter1.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager1
+            // 
+            this.tableAdapterManager1.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager1.CategoriesTableAdapter = this.categoriesTableAdapter1;
+            this.tableAdapterManager1.ProductsTableAdapter = this.productsTableAdapter1;
+            this.tableAdapterManager1.UpdateOrder = CategoryProducts.NorthwindDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(894, 522);
+            this.Controls.Add(this.lblProducts);
+            this.Controls.Add(this.lblCategories);
+            this.Controls.Add(this.lstProducts);
+            this.Controls.Add(this.lstCategories);
+            this.Name = "Form1";
+            this.Text = "Categories and Products";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstCategories;
+        private System.Windows.Forms.ListBox lstProducts;
+        private System.Windows.Forms.Label lblCategories;
+        private System.Windows.Forms.Label lblProducts;
+        private NorthwindDataSet northwindDataSet1;
+        private NorthwindDataSetTableAdapters.CategoriesTableAdapter categoriesTableAdapter1;
+        private NorthwindDataSetTableAdapters.ProductsTableAdapter productsTableAdapter1;
+        private NorthwindDataSetTableAdapters.TableAdapterManager tableAdapterManager1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CategoryProducts
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                categoriesTableAdapter1.Fill(
+                    northwindDataSet1.Categories);
+                productsTableAdapter1.Fill(
+                    northwindDataSet1.Products);
+                //Linq query
+                var categoryRows =
+                    from category in northwindDataSet1.Categories
+                    select category;
+                foreach (var item in categoryRows)
+                {
+                    lstCategories.Items.Add(item.CategoryID + " " +
+                        item.CategoryName);
+                }
+                
+                    
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Some errors occured during connection to the database" + ex);
+            }
+        }
+
+        private void lstCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                lstProducts.Items.Clear();
+                string cat = (string)lstCategories.SelectedItem;
+                int id = int.Parse(cat.Substring(0, 1));
+                lblProducts.Text = cat.Substring(1) + " 's products:";
+                //LINQ 
+                var productsResult =
+                    from productLine in northwindDataSet1.Products
+                    where productLine.CategoryID == id
+                    select productLine;
+                foreach (var item in productsResult)
+                {
+                    lstProducts.Items.Add(item.ProductID.ToString().PadLeft(3) +
+                        " " + item.ProductName);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Some errors occured" + ex.Message, "Error: ");
+            }
+        }
+    }
+}
